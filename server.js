b7a0c372a0b91b09fa95d718a92d04ab2ca79a79
annachatkara <downloadallmovies04@gmail.com>
2025-09-26 14:26:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -309,7 +309,5 @@ app.get("/", (_req, res) => {
   res.json({ ok: true });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+export default app;
+
